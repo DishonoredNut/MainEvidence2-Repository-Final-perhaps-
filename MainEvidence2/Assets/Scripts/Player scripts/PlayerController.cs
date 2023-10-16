a303a43f71fbd59a9ddc1bfac91c95190435d912
DishonoredNut <142398714+DishonoredNut@users.noreply.github.com>
2023-10-16 15:09:24 +0200

    Got gun workin

--- MainEvidence2/Assets/Scripts/Player scripts/PlayerController.cs
+++ MainEvidence2/Assets/Scripts/Player scripts/PlayerController.cs
@@ -19,8 +19,14 @@ public class PlayerController : MonoBehaviour
     private bool Grounded;
     public float teleportHeightThreshold = -10.0f; // Threshold height for teleportation
     public Transform teleportLocation; // Location to teleport the player to
-
     public GameObject objectToTeleport; // Reference to the object to teleport
+
+    public GameObject projectilePrefab; // Reference to the projectile prefab
+    public Transform firepoint; // Reference to the firepoint transform
+    public float fireRate = 0.5f; // Firing rate in bullets per second
+    private float lastFireTime; // Time of the last bullet fired
+    public AudioSource fireSound; // Reference to an AudioSource for the firing sound
+    public float bulletSpeed = 10f; // Speed of the fired bullet
 
     void Awake()
     {
@@ -44,6 +50,7 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
         Jump();
         CheckTeleport();
+        FireDaBullet(); // Call the fire bullet method
     }
 
     private void Gravity()
@@ -76,26 +83,56 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void CheckTeleport()
-{
-    if (transform.position.y <= teleportHeightThreshold)
+    private void FireDaBullet()
     {
-        // Teleport the specified object to the teleport location
-        objectToTeleport.transform.position = teleportLocation.position;
-
-        // Optionally, reset the player's position as well.
-        controller.enabled = false; // Disable the CharacterController temporarily
-        transform.position = teleportLocation.position;
-        velocity = Vector3.zero; // Reset the player's velocity
-
-        // Teleport all child objects
-        foreach (Transform child in objectToTeleport.transform)
+        if (controls.GamePadMovement.Fire.triggered && projectilePrefab != null && firepoint != null)
         {
-            child.position = teleportLocation.position;
+            // Check if enough time has passed to fire another bullet
+            if (Time.time - lastFireTime >= 1.0f / fireRate)
+            {
+                // Create the projectile at the firepoint's position and rotation
+                GameObject bullet = Instantiate(projectilePrefab, firepoint.position, firepoint.rotation);
+
+                // Access the bullet's Rigidbody component
+                Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    // Propel the bullet in the direction the firepoint is facing
+                    rb.velocity = firepoint.forward.normalized * bulletSpeed;
+                }
+
+                // Play the firing sound
+                if (fireSound != null)
+                {
+                    fireSound.Play();
+                }
+
+                // Update the last fire time
+                lastFireTime = Time.time;
+            }
         }
-
-        controller.enabled = true; // Re-enable the CharacterController
     }
-}
 
+    private void CheckTeleport()
+    {
+        if (transform.position.y <= teleportHeightThreshold)
+        {
+            // Teleport the specified object to the teleport location
+            objectToTeleport.transform.position = teleportLocation.position;
+
+            // Optionally, reset the player's position as well.
+            controller.enabled = false; // Disable the CharacterController temporarily
+            transform.position = teleportLocation.position;
+            velocity = Vector3.zero; // Reset the player's velocity
+
+            // Teleport all child objects
+            foreach (Transform child in objectToTeleport.transform)
+            {
+                child.position = teleportLocation.position;
+            }
+
+            controller.enabled = true; // Re-enable the CharacterController
+        }
+    }
 }
